ce1d1b8ea9f845a2ab60d73f25d1293db1e4c9b8
Adeen Bin Waheed <adeenwaheed@hotmail.com>
2025-04-14 16:34:28 +0500

    Initial Project Setup

---
+++ ConsoleAppStringProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppStringProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
