b342d8d226058d337803aeadc6017184727e1fd5
Adeen Bin Waheed <adeenwaheed@hotmail.com>
2025-07-02 01:33:56 +0500

    ReverseWordsInAStringSol problem solved

---
+++ ConsoleAppStringProblems/ReverseWordsInAStringSol.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace ConsoleAppStringProblems
+{
+    public class ReverseWordsInAStringSol
+    {
+        public void Code()
+        {
+            //Reverse words in a string
+            //Given an input string s, reverse the order of the words.
+            //Input: s = "the sky is blue"
+            //Output: "blue is sky the"
+
+            string s = "the sky is blue";
+            Console.WriteLine($"Reversed string: {ReverseWords(s)}");
+        }
+
+        private string ReverseWords(string s)
+        {
+            var words = s.Split(' ');
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                if (words[i] != "")
+                    sb.Append($"{words[i]} ");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
