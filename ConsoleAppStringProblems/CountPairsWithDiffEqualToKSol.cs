6fe37ddb276dd0bfb99c4c219eaf74d3a98e24f9
Adeen Bin Waheed <adeenwaheed@hotmail.com>
2025-07-30 13:48:46 +0500

    CountPairsWithDiffEqualToKSol problem solved

---
+++ ConsoleAppStringProblems/CountPairsWithDiffEqualToKSol.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleAppStringProblems
+{
+    public class CountPairsWithDiffEqualToKSol
+    {
+        public void Code()
+        {
+            //Problem: Count pairs with absolute difference equal to k.
+            //Output: 3-1, 5-3, 4-2
+            int[] arr = [3, 2, 1, 5, 4]; int k = 2;
+
+            //Logic idea
+            //arr[i] - arr[j] = k; -> arr[i] - k = arr[j];
+            //arr [i] - arr[j] = -k; -> arr[i] + k = arr[j];
+
+            int counter = 0;
+            HashSet<int> hasSet = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (hasSet.Contains(arr[i] - k))
+                    counter++;
+                else if (hasSet.Contains(arr[i] + k))
+                    counter++;
+                else
+                    hasSet.Add(arr[i]);
+            }
+
+            Console.WriteLine($"Pairs: {counter}, with absolute difference equal to k");
+        }
+
+    }
+}
--- ConsoleAppStringProblems/Program.cs
+++ ConsoleAppStringProblems/Program.cs
@@ -5,14 +5,16 @@
         //Call the code method of any problem class
         static void Main(string[] args)
         {
-            //RepeatCharactersSol2 repeatCharactersSol2 = new RepeatCharactersSol2();
-            //repeatCharactersSol2.Code();
-
+            //𝟏. 𝐓𝐰𝐨 𝐒𝐮𝐦: Hashing, Arrays
             //SumOfTwoNumsEqualsToTarget sumOfTwoNumsEqualsToTarget = new SumOfTwoNumsEqualsToTarget();
             //sumOfTwoNumsEqualsToTarget.Code();
 
+            //𝟏. 𝐓𝐰𝐨 𝐒𝐮𝐦: Hashing, Arrays
             //SumOfTwoNumsEqualsToTarget_TwoPointers sumOfTwoNumsEqualsToTarget_TwoPointers = new SumOfTwoNumsEqualsToTarget_TwoPointers();
             //sumOfTwoNumsEqualsToTarget_TwoPointers.Code();
+
+            //RepeatCharactersSol2 repeatCharactersSol2 = new RepeatCharactersSol2();
+            //repeatCharactersSol2.Code();
 
             //MostOccuringEleSol mostOccuringEleSol = new MostOccuringEleSol();
             //mostOccuringEleSol.Code();
@@ -20,11 +22,14 @@
             //LengthOfLongestSubstringSol lengthOfLongestSubstringSol = new LengthOfLongestSubstringSol();
             //lengthOfLongestSubstringSol.Code();
 
-            LengthOfLongestSubstringSol_TwoPointers lengthOfLongestSubstringSol_TwoPointers = new LengthOfLongestSubstringSol_TwoPointers();
-            lengthOfLongestSubstringSol_TwoPointers.Code();
+            //LengthOfLongestSubstringSol_TwoPointers lengthOfLongestSubstringSol_TwoPointers = new LengthOfLongestSubstringSol_TwoPointers();
+            //lengthOfLongestSubstringSol_TwoPointers.Code();
 
-            ReverseWordsInAStringSol reverseWordsInAStringSol = new ReverseWordsInAStringSol();
-            reverseWordsInAStringSol.Code();
+            //ReverseWordsInAStringSol reverseWordsInAStringSol = new ReverseWordsInAStringSol();
+            //reverseWordsInAStringSol.Code();
+
+            CountPairsWithDiffEqualToKSol countPairsWithDiffEqualToKSol = new CountPairsWithDiffEqualToKSol();
+            countPairsWithDiffEqualToKSol.Code();
         }
     }
 }
